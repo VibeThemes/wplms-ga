f29b9c5e5a8a1361950d7cb181c9ab832dc903a7
Ripul Kumar <vibethemes@gmail.com>
2017-04-12 11:52:53 +0530

    init

---
+++ loader.php
@@ -0,0 +1,26 @@
+<?php
+/*
+Plugin Name: WPLMS Google Analytics
+Plugin URI: http://www.Vibethemes.com
+Description: Integrate Google Analytics with WPLMS
+Version: 1.0
+Author: VibeThemes
+Author URI: http://www.vibethemes.com
+License: GPL2
+*/
+/*
+Copyright 2014  VibeThemes  (email : vibethemes@gmail.com)
+
+WPLMS GA program is free software; you can redistribute it and/or modify
+it under the terms of the GNU General Public License, version 2, as 
+published by the Free Software Foundation.
+
+WPLMS GA program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with WPLMS GA program; if not, write to the Free Software
+Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+*/
